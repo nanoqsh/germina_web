0f6348a730a74481fce82825bd9c28da7d0e9290
nanoqsh <nanoqsh@gmail.com>
2022-07-07 02:06:32 +0600

    Add entity

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,40 @@
 mod pool;
 
-use crate::pool::{run_migrations, Pool};
+use crate::pool::Db;
 use rocket::{
     fairing::AdHoc,
     fs::FileServer,
     get, routes,
     serde::{json::Json, Serialize},
 };
-use rocket_db_pools::{Connection, Database};
+use sea_orm_rocket::{Connection, Database};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct Message<'a> {
-    text: &'a str,
+struct Message {
+    users: Vec<String>,
 }
 
 #[get("/")]
-async fn index(_db: Connection<Pool>) -> Json<Message<'static>> {
-    let message = Message { text: "hello" };
-    message.into()
+async fn list(conn: Connection<'_, Db>) -> Json<Message> {
+    use entity::prelude::User;
+    use sea_orm::EntityTrait;
+
+    let db = conn.into_inner();
+    let users = User::find().all(db).await.expect("all users");
+
+    Json::from(Message {
+        users: users.into_iter().map(|user| user.name).collect(),
+    })
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .attach(Pool::init())
-        .attach(AdHoc::try_on_ignite("Run migrations", run_migrations))
+        .attach(Db::init())
+        .attach(AdHoc::try_on_ignite("Run migrations", pool::run_migrations))
         .mount("/", FileServer::from("static"))
-        .mount("/api", routes![index])
+        .mount("/api", routes![list])
         .launch()
         .await
         .expect("launch");
--- server/src/pool.rs
+++ server/src/pool.rs
@@ -1,17 +1,47 @@
-use rocket::{fairing, Build, Rocket};
-use rocket_db_pools::{sqlx, Database};
+use rocket::{async_trait, fairing, Build, Rocket};
+use sea_orm::{ConnectOptions, DatabaseConnection, DbErr};
+use sea_orm_rocket::{rocket::figment::Figment, Config, Database};
+use std::time::Duration;
 
 #[derive(Database)]
 #[database("sqlite")]
-pub struct Pool(sqlx::SqlitePool);
+pub struct Db(Pool);
+
+#[derive(Debug, Clone)]
+pub struct Pool {
+    conn: DatabaseConnection,
+}
+
+#[async_trait]
+impl sea_orm_rocket::Pool for Pool {
+    type Error = DbErr;
+    type Connection = DatabaseConnection;
+
+    async fn init(figment: &Figment) -> Result<Self, Self::Error> {
+        let config: Config = figment.extract().expect("extract");
+        let mut options: ConnectOptions = config.url.into();
+        options
+            .max_connections(config.max_connections as u32)
+            .min_connections(config.min_connections.unwrap_or_default())
+            .connect_timeout(Duration::from_secs(config.connect_timeout));
+        if let Some(idle_timeout) = config.idle_timeout {
+            options.idle_timeout(Duration::from_secs(idle_timeout));
+        }
+        let conn = sea_orm::Database::connect(options).await?;
+
+        Ok(Self { conn })
+    }
+
+    fn borrow(&self) -> &Self::Connection {
+        &self.conn
+    }
+}
 
 pub async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
-    use migration::{Migrator, MigratorTrait};
-    use sea_orm::SqlxSqliteConnector;
+    use migration::MigratorTrait;
 
-    let pool = Pool::fetch(&rocket).expect("fetch").0.clone();
-    let db = SqlxSqliteConnector::from_sqlx_sqlite_pool(pool);
-    Migrator::up(&db, None).await.expect("migration up");
+    let conn = &Db::fetch(&rocket).unwrap().conn;
+    let _ = migration::Migrator::up(conn, None).await;
 
     Ok(rocket)
 }
